e628655801acc25fa2d790bb3eeb971fe76d4042
sanaz-niamati <sanaz.niamati@yahoo.com>
2022-06-19 10:25:14 +0430

    first commit

--- src/index.js
+++ src/index.js
@@ -1,37 +1,49 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { Stage, Layer, Rect } from "react-konva";
+import { Stage, Layer, Line } from "react-konva";
 
 const App = () => {
   return (
     <Stage width={window.innerWidth} height={window.innerHeight}>
       <Layer>
-        <Rect
+        <Line
           x={20}
-          y={20}
-          width={100}
-          height={50}
-          fill="green"
-          stroke="black"
-          strokWidth={4}
+          y={5}
+          points={[5, 70, 140, 23, 250, 60, 300, 20]}
+          stroke={"red"}
+          strokeWidth={15}
+          lineCap={"round"}
+          lineJoin={"round"}
+          tension={1}
         />
-        <Rect
-          x={150}
-          y={40}
-          width={100}
-          height={50}
-          fill="red"
-          shadowBlur={10}
-          stroke="black"
-          cornerRadius={10}
+        {/*dashed line*/}
+        <Line
+          x={20}
+          y={55}
+          points={[5, 70, 140, 23, 250, 60, 300, 20]}
+          stroke={"green"}
+          strokeWidth={2}
+          lineJoin={"round"}
+          lineCap={"round"}
+          /* line segments with a length of 33px with a gap of 10px */
+          dash={[33, 10]}
+          tension={0.5}
         />
-        <Rect
-          x={50}
-          y={120}
-          width={100}
-          height={100}
-          fill="blue"
-          cornerRadius={[0, 10, 20, 30]}
+        {/*complex dashed and dotted line*/}
+        <Line
+          x={20}
+          y={105}
+          points={[5, 70, 140, 23, 250, 60, 300, 20]}
+          stroke={"blue"}
+          strokeWidth={10}
+          lineJoin={"round"}
+          lineCap={"round"}
+          /* line segments with a length of 29px with a gap
+           * of 20px followed by a line segment of 0.001px (a dot)
+           * followed by a gap of 20px
+           */
+          dash={[29, 20, 0.001, 20]}
+          tension={0.7}
         />
       </Layer>
     </Stage>
